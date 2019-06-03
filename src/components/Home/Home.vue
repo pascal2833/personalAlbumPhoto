073b06ffb1460b457d5a6996d43755c3a80473bb
pascal <pascal2833@gmail.com>
2019-06-03 13:00:00 +0200

    Import lateral menu

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,12 +1,74 @@
 <template>
     <div class="home">
+      <template>
+        <sidebar-menu :menu="menu" />
+      </template>
+      <template>
+        <photos-container></photos-container>
+      </template>
     </div>
 </template>
 
 <script>
 
+import { SidebarMenu } from 'vue-sidebar-menu'
+import photosContainer from '../photosContainer/photosContainer'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    SidebarMenu,
+    photosContainer
+  },
+  data () {
+    return {
+      menu: [
+        {
+          header: true,
+          title: 'Main Navigation'
+          // component: componentName
+          // visibleOnCollapse: true
+          // class:''
+          // attributes: {}
+        },
+        { // item
+          href: '/',
+          title: 'Dashboard',
+          icon: 'fa fa-user'
+          /*
+          // custom icon
+          icon: {
+              element: 'span',
+              class: 'fa fa-user',
+              attributes: {}
+          }
+          */
+          // disabled: true
+          // class: ''
+          // attributes: {}
+          // alias: '/path'
+          /*
+          badge: {
+              text: 'new',
+              class: 'default-badge'
+              // attributes: {}
+              // element: 'span'
+          }
+          */
+        },
+        { // item with child
+          href: '/charts',
+          title: 'Charts',
+          icon: 'fa fa-chart-area',
+          child: [
+            {
+              href: '/charts/sublink',
+              title: 'Sub Link'
+            }
+          ]
+        }
+      ]
+    }
+  }
 }
 </script>
 
---
+++ src/components/photosContainer/photosContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="photos-container">
+    jfvorkjfoi
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'photosContainer'
+}
+
+</script>
+
+<style lang="scss" scoped src="./photosContainer.scss"></style>
